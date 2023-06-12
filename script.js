12f4b6e20aa157ca99d5c70730bb751ba1d5c69c
Dexter Kong <78414602+dexico-gme@users.noreply.github.com>
2023-06-12 23:43:49 +0100

    modified:   index.html 	modified:   script.js 	modified:   style.css
    added calculations in javascript to work out area of pixels required
    
    added clear function to clear button
    
    added css to style etch-a-sketch like gui

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const container = document.querySelector(".container");
 
+function calculateDiv(num) {
+  canvasSize = 480 * 480;
+  square = Math.sqrt(canvasSize / (num * num));
+  return square;
+}
+
 function grids(num) {
   let numGrids = num * num;
   console.log(numGrids);
@@ -10,14 +16,32 @@ function grids(num) {
   }
 }
 
-grids(100);
+let userInput = 16;
+grids(userInput);
 
 let allDivs = document.querySelectorAll(".etchDiv");
 
+function rainbow() {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  let newcolor = "#" + randomColor;
+}
+
 allDivs.forEach((box) => {
+  calculateDiv(userInput);
+  console.log(`square value = ${square}`);
+  box.style.width = `${square}px`;
+  box.style.height = `${square}px`;
   box.onmouseover = () => {
-    box.style.backgroundColor = "red";
+    box.style.backgroundColor = "grey";
   };
 });
 // const newDiv = document.createElement("div");
 // container.appendChild(newDiv);
+function clear() {
+  allDivs.forEach((box) => {
+    box.style.backgroundColor = "lightgrey";
+    console.log("cleared");
+  });
+}
+
+document.getElementById("clear").addEventListener("click", clear);
