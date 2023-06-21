ddfcb5474c6a71a1d884ced89d0a5c4585c4ee95
Dexter Kong <78414602+dexico-gme@users.noreply.github.com>
2023-06-21 23:55:49 +0100

    modified:   index.html 	modified:   script.js 	modified:   style.css
    added rainbow & clear function
    refactored javascript code to calculate correct area for 480x480 grid

--- script.js
+++ script.js
@@ -1,47 +1,88 @@
 const container = document.querySelector(".container");
 
-function calculateDiv(num) {
-  canvasSize = 480 * 480;
-  square = Math.sqrt(canvasSize / (num * num));
-  return square;
+// function calculateDiv(num) {
+//   let canvasSize = 480 * 480;
+//   let square = Math.sqrt(canvasSize / (num * num));
+//   return square;
+// }
+
+function run(num = 10) {
+  var num = document.getElementById("myText").value;
+  console.log(num);
+  grids(num);
 }
 
-function grids(num) {
-  let numGrids = num * num;
-  console.log(numGrids);
-  for (x = 1; x < numGrids + 1; x++) {
-    const newDiv = document.createElement("div");
-    newDiv.className = "etchDiv";
-    container.appendChild(newDiv);
+grids(16);
+
+function grids(num = 10) {
+  let etchDiv = document.querySelectorAll(".etchDiv");
+  if (etchDiv != null) {
+    etchDiv.forEach((box) => {
+      box.remove();
+      console.log("cleared");
+    });
+
+    let numGrids = num * num;
+    let canvasSize = 480 * 480;
+    let square = Math.sqrt(canvasSize / (num * num));
+    console.log("numgrids" + numGrids);
+    for (x = 1; x < numGrids + 1; x++) {
+      const newDiv = document.createElement("div");
+      newDiv.className = "etchDiv";
+      container.appendChild(newDiv);
+      console.log("square" + square);
+    }
+    let allDivs = document.querySelectorAll(".etchDiv");
+    allDivs.forEach((box) => {
+      console.log(`square value = ${square}`);
+      box.style.width = `${square}px`;
+      box.style.height = `${square}px`;
+      box.onmouseover = () => {
+        box.style.backgroundColor = "grey";
+      };
+    });
+    console.log("done");
   }
 }
 
-let userInput = 16;
-grids(userInput);
+// console.log("userInput" + userInput);
+//minimum 16;
 
-let allDivs = document.querySelectorAll(".etchDiv");
+// console.log("user input" + x);
+document.getElementById("rainbow").addEventListener("click", rainbow);
 
 function rainbow() {
-  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  let newcolor = "#" + randomColor;
+  let colorDiv = document.querySelectorAll(".etchDiv");
+  colorDiv.forEach((color) => {
+    color.onmouseover = () => {
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      let rainbowColor = "#" + randomColor;
+      color.style.backgroundColor = rainbowColor;
+    };
+  });
 }
 
-allDivs.forEach((box) => {
-  calculateDiv(userInput);
-  console.log(`square value = ${square}`);
-  box.style.width = `${square}px`;
-  box.style.height = `${square}px`;
-  box.onmouseover = () => {
-    box.style.backgroundColor = "grey";
-  };
-});
-// const newDiv = document.createElement("div");
-// container.appendChild(newDiv);
+document.getElementById("colorpick").addEventListener("input", colorPick);
+
+function colorPick() {
+  let newColor = document.getElementById("colorpick").value;
+  colorDiv.forEach((color) => {
+    color.onmouseover = () => {
+      color.style.backgroundColor = newColor;
+    };
+  });
+}
+
+const newDiv = document.createElement("div");
+container.appendChild(newDiv);
+
+document.getElementById("clear").addEventListener("click", clear);
+
 function clear() {
+  console.log("clear");
+  let allDivs = document.querySelectorAll(".etchDiv");
   allDivs.forEach((box) => {
     box.style.backgroundColor = "lightgrey";
     console.log("cleared");
   });
 }
-
-document.getElementById("clear").addEventListener("click", clear);
